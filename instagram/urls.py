5c26991bf34dea2c6c98d671c211c9225db84b11
Mercynenoh <mercychepngenoh74@gmail.com>
2022-06-09 15:54:27 +0300

    Add logic to display post images

--- instagram/urls.py
+++ instagram/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from photos.views import ImageList
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('', include('photos.urls')),
     path('', ImageList.as_view(), name='list'),
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
