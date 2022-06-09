7520213a1b7626fe0b26715cdca2b61e62cee291
Mercynenoh <mercychepngenoh74@gmail.com>
2022-06-09 16:04:27 +0300

    Add view to View single photo

--- photos/views.py
+++ photos/views.py
@@ -21,6 +21,15 @@ def image_list(request):
     posts = models.Post.objects.all()
     return render(request, "photos/post_list.html", {'post':post})
 
+def viewPhoto(request, pk):
+    post = Post.objects.get(id=pk)
+    if request.method == 'POST':
+        value = request.POST['value']
+        comments = request.POST['comments']
+        if value == 'comments':
+            comments.save()
+    return render(request, 'photos/image.html', {'post':post})
+
 # class ProfileList( ListView):
 #     model = Profile
 #     template_name = 'photos/profile_list.html'
