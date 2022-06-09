fb917d8ca7206f565291059e72b0cf628cff600a
Mercynenoh <mercychepngenoh74@gmail.com>
2022-06-09 15:28:28 +0300

    Add image list class to display posts

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect,get_object_or_404
+from django.http  import HttpResponse, Http404
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView
+from .models import Post, Profile
+from django import template
+from django.db import models
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+class ImageList(ListView):
+    model = Post
+    template_name = 'photos/post_list.html'
+
+    def get_queryset(self):
+        return Post.objects.all()
+
+# class ProfileList( ListView):
+#     model = Profile
+#     template_name = 'photos/profile_list.html'
+
+#     def get_queryset(self):
+#         return Profile.objects.all()
