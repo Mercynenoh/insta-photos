71b61f86fefa0ae689e32395564efe3c8cfbb551
Mercynenoh <mercychepngenoh74@gmail.com>
2022-06-09 14:49:48 +0300

    Create a profile model

--- photos/models.py
+++ photos/models.py
@@ -3,7 +3,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    pic = models.ImageField(upload_to = 'articles/',default='IMAGE')
+    bio = models.TextField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE,default='LOCATION')
+    
+    # def save_image(self):
+    #     self.save()
 
+    # def save_bio(self):
+    #     self.save()
+
+    def __str__(self):
+        return self.bio
+        
 class Post(models.Model):
     image = models.ImageField(upload_to = 'articles/',default='IMAGE')
     imagename =  models.CharField(max_length =30)
